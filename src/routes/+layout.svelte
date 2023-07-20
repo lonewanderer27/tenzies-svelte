02c4f56b9b7af5978c6e1103d2df5634069556aa
lonewanderer27 <adrianejames27@gmail.com>
2023-07-20 18:10:24 +0800

    add layout component

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<script>
+
+</script>
+
+<header>
+  <a href="/about">
+    <button>About</button>
+  </a>
+  <a href="/leaderboards">
+    <button>Leaderboards</button>
+  </a>
+  <a href="/profile">
+    <button>Profile</button>
+  </a>
+</header>
+
+
+<main>
+  <slot/>
+</main>
